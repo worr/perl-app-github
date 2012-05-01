11ee19584deb6f0c8ada8e7850c9d5b30afd9d78
Will Orr <will@worrbase.com>
2012-05-01 15:07:21 -0400

    Docs

--- scripts/github.pl
+++ scripts/github.pl
@@ -9,6 +9,7 @@ use strict;
 use warnings;
 
 use App::GitHub;
+use Pod::Usage;
 
 use Getopt::Long;
 Getopt::Long::Configure("bundling");
@@ -19,17 +20,21 @@ if (@ARGV == 0) {
     App::GitHub->new->run(@ARGV);
 } else {
     # Let's define some options!
-    my ($create, $username, $password, $key, $name, $fork);
+    my ($create, $username, $password, $key, $name, $fork, $help);
     GetOptions(
         'create|c=s'        => \$create,
         'key|k=s'           => \$key,
         'name|n=s'          => \$name,
         'fork|f=s'          => \$fork,
         'username|u=s'      => \$username,
-        'password|p=s'      => \$password
+        'password|p=s'      => \$password,
+        'help'              => \$help,
     );
 
     my $github = App::GitHub->new( silent => 1 );
+    if ($help) {
+        pod2usage(1);
+    }
 
     unless ($username and $password) {
         $github->set_loadcfg;
@@ -76,3 +81,64 @@ if (@ARGV == 0) {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+github.pl - Interact with github.com through the command line
+
+=head1 SYNOPSIS
+
+github.pl [options]
+
+When run with no options, drops you to the github command line.
+
+If --username and --password aren't passed in, github.user and github.pass from .gitconfig are used
+
+Options:
+
+    --username='username'
+    --password='password'
+    --create='name of repo to create'
+    --key='pubkey' --name='name of key'
+    --fork='name of repo to fork'
+    --help
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--create>
+
+Creates a new repo on github
+
+=item B<--key --name>
+
+Adds a key with a given name (passed by --name) to a github account
+
+=item B<--fork>
+
+Fork the given github repo to your account
+
+=item B<--help>
+
+Print help text and exit
+
+=item B<--username>
+
+Github username 
+
+=item B<--password>
+
+Github password
+
+=back
+
+=head1 AUTHOR
+
+William Orr <will@worrbase.com>
+
+Please report bugs L<here|https://github.com/worr/perl-app-github/>
+
+=cut
