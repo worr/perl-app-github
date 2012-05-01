82098c9827242b531d278c052b57999f2f882c4d
Will Orr <will@worrbase.com>
2012-05-01 14:29:55 -0400

    Can fork!
    Also better error handling

--- scripts/github.pl
+++ scripts/github.pl
@@ -19,11 +19,12 @@ if (@ARGV == 0) {
     App::GitHub->new->run(@ARGV);
 } else {
     # Let's define some options!
-    my ($create, $username, $password, $key, $name);
+    my ($create, $username, $password, $key, $name, $fork);
     GetOptions(
         'create|c=s'        => \$create,
         'key|k=s'           => \$key,
         'name|n=s'          => \$name,
+        'fork|f=s'          => \$fork,
         'username|u=s'      => \$username,
         'password|p=s'      => \$password
     );
@@ -42,21 +43,34 @@ if (@ARGV == 0) {
         };
 
         if ($@) {
-            die "Could not create repo $create";
+            say STDERR "Could not create repo $create";
+            print STDERR $@;
         } else {
             say "Created repo $create";
         }
     } elsif ($key) {
-        die "Provide a name for the key with -n" if not $name;
+        say STDERR "Provide a name for the key with -n" if not $name;
 
         eval {
             $github->user_pub_keys("add", $name, $key);
         };
 
         if ($@) {
-            die "Could not add key";
+            say STDERR "Could not add key";
+            print STDERR $@;
         } else {
             say "Added pubkey";
+        }
+    } elsif ($fork) {
+        eval {
+            $github->run_basic_repo_cmd( 'repos', 'create_fork', $fork);
+        };
+
+        if ($@) {
+            say STDERR "Could not fork $fork";
+            print STDERR $@;
+        } else {
+            say "Forked repo $fork";
         }
     }
 }
